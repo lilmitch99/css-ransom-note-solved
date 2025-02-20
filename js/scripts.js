750a68ff3ba1fbe13f92eb91ef8c2a7b5673a82c
lilmitch99 <lilmitch99@gmail.com>
2025-02-20 13:32:49 -0600

    Completed all required tasks, updated README.md, formatted classes in the index file as I saw fit, styled all letters in the CSS file

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ const fontSizeArr = [
   48, 42, 48, 45, 44,
 ];
 
-const fontsArr = ["Piazzolla", "Ceviche One", "Raleway Dots"];
+const fontsArr = ["Piazzolla", "Ceviche One", "Raleway Dots", "Rubik Moonrocks", "Rubik Glitch", "Sixtyfour"];
 
 const textDecorationArr = ["underline", "line-through", "overline", "none"];
 const textTransformArr = ["uppercase", "lowercase"];
